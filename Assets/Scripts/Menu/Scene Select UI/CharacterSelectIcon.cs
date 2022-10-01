8e1014ad8520b1d359af8b1bb7b50ef119912f4c
thothbubble <thothbubble@gmail.com>
2022-10-01 16:36:26 -0400

    Character Select Screen

---
+++ Assets/Scripts/Menu/Scene Select UI/CharacterSelectIcon.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelectIcon : MonoBehaviour
+{
+    public enum CharacterChoice { THIEF, HACKER, GAMBLER, CAVEMAN, CYBER, FIGHT, FINAL }
+
+    [SerializeField] private Image icon;
+
+    void Start()
+    {
+        
+    }
+
+    public void UpdateIcon(Sprite s)
+    {
+        icon.sprite = s;
+    }
+}
