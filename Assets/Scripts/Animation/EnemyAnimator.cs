1c885b4a375901c971dbf8695e2fb9991ac3cf33
nshooter <nick.shooter@gmail.com>
2022-10-02 01:44:50 -0400

    battle anims

--- Assets/Scripts/Animation/EnemyAnimator.cs
+++ Assets/Scripts/Animation/EnemyAnimator.cs
@@ -8,27 +8,6 @@ public class EnemyAnimator : MonoBehaviour
     [SerializeField]
     private DamageFlicker damageFlicker;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Z))
-        {
-            Attack();
-        }
-        else if (Input.GetKeyDown(KeyCode.X))
-        {
-            Spell();
-        }
-        else if (Input.GetKeyDown(KeyCode.C))
-        {
-            Hurt();
-        }
-        else if (Input.GetKeyDown(KeyCode.V))
-        {
-            Die();
-        }
-    }
-
     public void Attack()
     {
         anim.SetTrigger("Attack");
