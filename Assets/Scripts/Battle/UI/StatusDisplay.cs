88aaa583cd8313527dda20cbccdd9c684d2ba261
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2022-10-02 23:09:58 -0400

    Animated status icons in panel

--- Assets/Scripts/Battle/UI/StatusDisplay.cs
+++ Assets/Scripts/Battle/UI/StatusDisplay.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,37 @@ public class StatusDisplay : MonoBehaviour
 {
     [SerializeField]
     private List<Image> statusIcons = new List<Image>();
+    private const float ICON_ANIMATION_RATE = 0.1f;
+
+    private List<StatusSpriteCounter> spriteCounters = new List<StatusSpriteCounter>();
+
+    class StatusSpriteCounter
+    {
+        public Type statusType;
+        public List<Sprite> sprites;
+        public float spriteCounter;
+        public Image statusIcon;
+
+        public StatusSpriteCounter()
+        {
+        }
+
+        public StatusSpriteCounter(Type statusType, List<Sprite> sprites, int spriteCounter, Image statusIcon)
+        {
+            this.statusType = statusType;
+            this.sprites = sprites;
+            this.spriteCounter = spriteCounter;
+            this.statusIcon = statusIcon;
+        }
+    }
+
+    private void Start()
+    {
+        foreach (var statusIcon in statusIcons)
+        {
+            spriteCounters.Add(new StatusSpriteCounter());
+        }
+    }
 
     public void DisplayStatus(List<Status> activeStatuses)
     {
@@ -15,12 +47,37 @@ public class StatusDisplay : MonoBehaviour
             statusIcon.gameObject.SetActive(false);
         }
 
-        int statusIconIndex = 0;
-        foreach (var status in activeStatuses)
+        for (int i = 0; i < activeStatuses.Count; i++)
         {
-            statusIcons[statusIconIndex].gameObject.SetActive(true);
-            statusIcons[statusIconIndex].sprite = status.statusSprite;
-            ++statusIconIndex;
+            if (i < activeStatuses.Count)
+            {
+                Status status = activeStatuses[i];
+                Image statusIcon = statusIcons[i];
+                statusIcon.gameObject.SetActive(true);
+                if (spriteCounters[i].statusType != activeStatuses[i].GetType())
+                {
+                    statusIcon.sprite = status.statusSprites[0];
+                    spriteCounters[i] = new StatusSpriteCounter(status.GetType(), status.statusSprites, 0, statusIcon);
+                }
+            }
+            else
+            {
+                spriteCounters[i] = new StatusSpriteCounter();
+            }
+        }
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < spriteCounters.Count; i++)
+        {
+            StatusSpriteCounter spriteCounter = spriteCounters[i];
+            if (spriteCounter.statusType == null)
+            {
+                continue;
+            }
+            spriteCounter.statusIcon.sprite = spriteCounter.sprites[(int)(spriteCounter.spriteCounter / ICON_ANIMATION_RATE) % spriteCounter.sprites.Count];
+            spriteCounter.spriteCounter += Time.deltaTime;
         }
     }
 }
